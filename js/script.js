3c78c5b3f06a56578231554b398cde6d17832506
maxswebservices <93405617+maxswebservices@users.noreply.github.com>
2022-04-21 22:06:01 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-//Mobile support (for navbar)
-
 function navToggle() {
     var b = document.getElementById("navBar");
     "navbar" === b.className ? b.className += " responsive" : b.className="navbar";
